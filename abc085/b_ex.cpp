e4d62ffbd7223953154d3313c750fe63eb7f3a14
o8n <mywaymasahiro5862sook@gmail.com>
2022-09-11 14:52:38 +0900

    p187まで終わり(

---
+++ abc085/b_ex.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+
+  set<int> S;
+  for (int i = 0; i < N; ++i) {
+    int d;
+    cin >> d;
+    S.insert(d);
+  }
+  cout << S.size() << endl;
+}
