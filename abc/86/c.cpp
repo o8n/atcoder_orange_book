9b34e64f7ff9db109249378c197f71009b5855c8
o8n <mywaymasahiro5862sook@gmail.com>
2022-12-28 00:57:17 +0900

    86 c traveling

--- abc/86/c.cpp
+++ abc/86/c.cpp
@@ -12,12 +12,24 @@ using ll = long long;
 using namespace std;
 
 int n;
-int ans = 0;
-long long t;
+int t[110000], x[110000], y[110000];
 
 int main() {
   cin >> n;
+  t[0]=x[0]=y[0]=0;
+  rep(i,n) cin >> t[i+1] >> x[i+1] >> y[i+1];
 
-  cout << ans << endl;
+  bool can = true;
+  rep(i,n) {
+    int dt = t[i+1] - t[i];
+    int dist = abs(x[i+1]-x[i]) + abs(y[i+1]-y[i]);
+    if (dist > dt) can = false;
+    if (dist % 2 != dt % 2) can = false;
+  }
+
+  if (can) cout << "Yes" << endl;
+  else cout << "No" << endl;
   return 0;
 }
+
+// xi+yiは毎秒ごとに偶数奇数が入れ替わる。よってdtが偶数ならdistも偶数である
